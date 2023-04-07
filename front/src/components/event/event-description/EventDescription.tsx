1f8e0974935b51ec1fe8be5ce5435a49b54adc29
ryanfrynelson13 <ryanfrynelson@hotmail.fr>
2023-04-07 15:47:55 +0200

    navbar

--- front/src/components/event/event-description/EventDescription.tsx
+++ front/src/components/event/event-description/EventDescription.tsx
@@ -1,8 +1,13 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { DetailledEvent } from "../../../types/events/detailled-event.type";
 import classes from './event-description.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faHeart as faHeartFull } from '@fortawesome/free-solid-svg-icons'
+import {faHeart} from '@fortawesome/free-regular-svg-icons'
 
 const EventDescription = ({imageUrl, intro, description, reviews}: DetailledEvent) => {
+
+    const [liked, setLiked] = useState(false)
 
     const stars = useMemo(() => {
         return reviews.length >0 ? reviews.reduce((total, {stars}) => {
@@ -17,9 +22,27 @@ const EventDescription = ({imageUrl, intro, description, reviews}: DetailledEven
                     <div className={classes['event-image']}>
                         <img src={imageUrl} alt="event" />
                     </div>
-                    <div>
-                        <p>{intro}</p>
-                        <p className={classes.stars}>{stars.toFixed(1)} 💖 ({reviews.length})</p>
+                    <div className={classes.side}>
+                        <div>
+                            {
+                                liked ? 
+                                <FontAwesomeIcon style={{
+                                    borderRadius: '50%',
+                                    cursor: 'pointer',
+                                    fontSize: '1.8rem'
+                                }} icon={faHeartFull} color="red" border={true} onClick={() => setLiked(liked => !liked)}/>
+                                :
+                                <FontAwesomeIcon style={{
+                                    borderRadius: '50%',
+                                    cursor: 'pointer',
+                                    fontSize: '1.8rem'
+                                }} icon={faHeart} border={true} onClick={() => setLiked(liked => !liked)}/>
+                            }
+                        </div>
+                        <div className={classes.intro}>
+                            <p>{intro}</p>
+                            <p className={classes.stars}>{stars.toFixed(1)} <FontAwesomeIcon icon={faStar} color="gold"/> ({reviews.length})</p>
+                        </div>
                     </div>
                 </div>
                 <div className={classes.desc}>
--- front/src/components/navbar/Navbar.tsx
+++ front/src/components/navbar/Navbar.tsx
@@ -4,6 +4,8 @@ import useSearch from '../../hooks/events/useSearch'
 import { useEffect, useState } from 'react'
 import { EventType } from '../../types/events/event.type'
 import Dropdown from '../dropdown/Dropdown'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBasketShopping, faUser} from '@fortawesome/free-solid-svg-icons'
 
 
 const Navbar = () => {
@@ -20,16 +22,60 @@ const Navbar = () => {
                 </NavLink>
             </div>
             <div>
-
+                <NavLink 
+                    to='' 
+                    className={({isActive}) => isActive ? classes.active : undefined }
+                >
+                    Home
+                </NavLink>
+                <NavLink 
+                    to='/categories' 
+                    className={({isActive}) => isActive ? classes.active : undefined }
+                >
+                    Categories
+                </NavLink>
+                <NavLink 
+                    to='/trending'
+                    className={({isActive}) => isActive ? classes.active : undefined }
+                >
+                    Trending
+                </NavLink>
             </div>
-            <div className={classes.search}>
-                <form>
-                    <div>
-                        <input onFocus={() => setInputFocus(true)} onBlur={() => setInputFocus(false)} type="text" autoComplete='off' name="search" id="search" placeholder='Events...' value={search} onChange={(e) => setSearch(e.target.value)}/>
-                        <button type='submit'>Search</button>
-                    </div>
-                    {events.length > 0 && inputFocus && <Dropdown items={events}/>}
-                </form>
+            <div className={classes['nav-right']}>
+                <div className={classes.search}>
+                    <form>
+                        <div>
+                            <input onFocus={() => setInputFocus(true)} onBlur={() => setInputFocus(false)} type="text" autoComplete='off' name="search" id="search" placeholder='Events...' value={search} onChange={(e) => setSearch(e.target.value)}/>
+                            <button type='submit'>Search</button>
+                        </div>
+                        {events.length > 0 && inputFocus && <Dropdown items={events}/>}
+                    </form>
+                </div>          
+                <div className={classes.icon}>
+                    <FontAwesomeIcon 
+                        style={{
+                            borderRadius: '50%',
+                            cursor: 'pointer',
+                            fontSize: '1.8rem',
+                            padding: '6px 6px'
+                        }} 
+                        border={true}
+                        icon={faBasketShopping}
+                    />
+                </div>
+                <div className={classes.icon}>
+                    <FontAwesomeIcon
+                        style={{
+                            borderRadius: '50%',
+                            cursor: 'pointer',
+                            fontSize: '1.8rem',
+                            padding: '6px 12px'
+                        }} 
+                        border={true}
+                        icon={faUser}
+                    />
+                </div>
+           
             </div>
         </nav>
     )
